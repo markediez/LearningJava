018ce72fa447949bdbf956b0cbbc1850ae7b012f
Mark Diez <markediez@gmail.com>
2015-11-21 01:45:07 -0800

    Add Hello World Program, Welcome1

---
+++ 2/Welcome1.java
@@ -0,0 +1,7 @@
+// This program is copied from the textbook
+
+public class Welcome1 {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java Programming!");
+    }
+}
