91bf7e51706a53675aaa3ba65a334bb5e461b546
Mark Diez <markediez@gmail.com>
2015-11-22 13:36:27 -0800

    Finish  set up linux java :D Add intro to Java GUI with Swing

---
+++ 3/Dialog1.java
@@ -0,0 +1,13 @@
+/*
+ * Code from text
+ * Program shows java dialog box
+ */
+
+import javax.swing.JOptionPane;
+
+public class Dialog1 {
+	public static void main(String[] args) {
+		// display message
+		JOptionPane.showMessageDialog(null, "Welcome to Java");
+	}
+}
