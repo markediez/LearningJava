d1e6b4d7461057dcdbe7a0ce8b8dfd8a5a1b44a3
Mark Diez <markediez@gmail.com>
2015-11-21 23:58:50 -0800

    Compile Account Note how to compile w/ personal classes Note static and new keyword

--- 3/AccountTest.java
+++ 3/AccountTest.java
@@ -24,6 +24,6 @@ public class AccountTest {
 		myAccount.setName(theName);
 		System.out.println(); // output blank line
 
-		System.out.printf("Initial name is: %s%n%n", myAccount.getName());
+		System.out.printf("Name in object myAccount is %n%s%n", myAccount.getName());
 	}
 }
