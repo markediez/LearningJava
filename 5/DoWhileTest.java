d6e353e05734412b1d7b9f39e969126178c00024
Mark Diez <markediez@gmail.com>
2015-11-29 11:29:49 -0800

    finish switch statement finish dowhile loops update code tips for end-of-file flag update readme

---
+++ 5/DoWhileTest.java
@@ -0,0 +1,17 @@
+/*
+ * Text Code
+ * Do While test
+ */
+
+public class DoWhileTest {
+    public static void main(String[] args) {
+        int counter = 1;
+
+        do {
+            System.out.printf("%d ", counter);
+            ++counter;
+        } while (counter <= 10);
+
+        System.out.println();
+    }
+}
